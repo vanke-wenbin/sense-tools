efd8e2ace01ba14934f33aa51f14253d67079747
B05FG36.刘文彬.Bin <liuwb06@vanke.net.cn>
2017-12-19 16:35:46 +0800

    export getDomainByBucketName

--- config.js
+++ config.js
@@ -1,7 +1,17 @@
 'use strict';
 
-const { BUCKET_DOMAIN } = require('/config/qiniu');
+const BUCKET_DOMAIN = {
+  'black-pearl': 'img2.4009515151.com',
+  'vanke-app': 'img.4009515151.com',
 
-module.exports = {
-  BUCKET_DOMAIN,
+  'uiis-test': 'p177rvoii.bkt.clouddn.com',
+  'fg-test': 'p177h8nuu.bkt.clouddn.com',
+};
+
+module.exports.getQiniuDomainByBucketName = function(bucketName) {
+  const domain = BUCKET_DOMAIN[bucketName];
+  if (!domain) {
+    throw new Error('Bucket ' + bucketName + ' is not registered.');
+  }
+  return domain;
 };
